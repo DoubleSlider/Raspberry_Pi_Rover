79e4d2f002bb821f1583db6bd35121d1a3149736
DoubleSlider <aeoncrown@gmail.com>
2016-08-17 20:22:40 +0800

    Update app.js
    add  obstacle detect and protect

--- app.js
+++ app.js
@@ -8,41 +8,52 @@ var app = require('http').createServer(handler)
   // make web server listen on port 1337
 app.listen(1337);
 
-//var SerialPort = require("serialport").SerialPort;
-board = new five.Board();/*{
-  port: new SerialPort("COM4", {
-    baudrate: 57600,
-    buffersize: 1
-  });*/
+board = new five.Board();
     
 var esc;
 var speed, steer;
+var speed_max = 100;
+var speed_min = 0;
+var safe_distance = 100; //cm
+var safe_speed = 20; //50+-20 = 30 and 70
 var IsBrake;
 // on board ready
 board.on("ready", function() {
 	// 利用前後左右控制汽車以一檔行進的方向, button A is for throtte, button X is for brake
 	var start = Date.now();
-
+	var SR04_1 = new five.Proximity({
+    		controller: "HCSR04",
+    		pin: 3
+    	});
+   	
+   	var SR04_2 = new five.Proximity({
+    		controller: "HCSR04",
+    		pin: 4
+  	});
+	
+	SR04_1.on("data", function() {
+		obstacle_distance_1 = this.cm;
+  	});
+	SR04_2.on("data", function() {
+		obstacle_distance_2 = this.cm;
+	});
 	
 	esc = new five.ESC({
 		device:"FORWARD_REVERSE",
 		neutral: 50,
 		pin: 9
 	});
-	//waiting for arming, is RC car require??
- //if (Date.now() - start < 2e3) {
-      //return;
-    //}
-  // init a led on pin 13, strobe every 1000ms
-  led = new five.Led(13).strobe(1000);
 
-  // setup a stanard servo, center at start
-  servo = new five.Servo({
-    pin:6,
-    range: [40,140],
-    type: "standard",
-    center:true
-  });
+	// init a led on pin 13, strobe every 1000ms
+	led = new five.Led(13).strobe(1000);
+	
+	// setup a stanard servo, center at start
+	servo = new five.Servo({
+		pin:6,
+		range: [40,140],
+		type: "standard",
+		center:true
+  	});
 
   // poll this sensor every second
   sensor = new five.Sensor({
@@ -69,9 +80,35 @@ function handler (req, res) {
   });
 }
 
-function calcSpeed(throtte, IsBrake){
+function calcSpeed(throtte, IsBrake, dist1, dist2){
 	//throtte = -50~50
+	
+	if(dist1 > safe_distance && dist2 > safe_distance){
+		// safe
+		speed_max = 100;
+		speed_min = 0;
+	}else{
+		if(dist1 < 10){
+			speed_max = 50+safe_speed;
+		}else if(dist1 < safe_distance){
+			speed_max = 50+safe_speed;
+			
+		}
+		
+		if((dist2 < 10){
+			speed_min = 50-safe_speed;
+		}else if(dist2 < safe_distance){
+			speed_min = 50-safe_speed;
+		}
+	}
+		
 	speed = (IsBrake-1)*throtte+50;
+	if( 50 > speed && speed < speed_min){
+		speed = speed_min;
+	}else if(speed > 50 && speed > speed_max){
+		speed = speed_max;
+	}
+	
 	return speed;
 }
 // on a socket connection

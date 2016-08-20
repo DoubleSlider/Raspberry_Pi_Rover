e9813ad88875e8469809b4dfe589a3c06fe0c298
DoubleSlider <aeoncrown@gmail.com>
2016-08-21 01:05:51 +0800

    Update app.js
    Arduino firmata is no longer been used in this project.
    
    app.js is modified to communicate with new firmware.

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 // initialize everything, web server, socket.io, filesystem, johnny-five
 var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
-  , fs = require('fs')
-  , five = require("johnny-five"),
-  board,servo,led,sensor;
+  , fs = require('fs');
 
   // make web server listen on port 1337
 app.listen(1337);
 
-board = new five.Board();
+var SerialPort = require("serialport");
+var serialport = new SerialPort("COM10",{
+	baudrate:57600,
+	parser:SerialPort.parsers.readline("\n")
+});
+
+
     
 var esc;
 var speed, steer;
@@ -17,54 +21,14 @@ var speed_min = 0;
 var safe_distance = 100; //cm
 var safe_speed = 20; //50+-20 = 30 and 70
 var IsBrake;
-// on board ready
-board.on("ready", function() {
-	// 利用前後左右控制汽車以一檔行進的方向, button A is for throtte, button X is for brake
-	var start = Date.now();
-	var SR04_1 = new five.Proximity({
-    		controller: "HCSR04",
-    		pin: 3
-    	});
-   	
-   	var SR04_2 = new five.Proximity({
-    		controller: "HCSR04",
-    		pin: 4
-  	});
-	
-	SR04_1.on("data", function() {
-		obstacle_distance_1 = this.cm;
-  	});
-	SR04_2.on("data", function() {
-		obstacle_distance_2 = this.cm;
-	});
-	
-	esc = new five.ESC({
-		device:"FORWARD_REVERSE",
-		neutral: 50,
-		pin: 9
-	});
 
-	// init a led on pin 13, strobe every 1000ms
-	led = new five.Led(13).strobe(1000);
-	
-	// setup a stanard servo, center at start
-	servo = new five.Servo({
-		pin:6,
-		range: [40,140],
-		type: "standard",
-		center:true
-  	});
-
-  // poll this sensor every second
-  sensor = new five.Sensor({
-    pin: "A0",
-    freq: 1000
+// arduino board
+serialport.on('open', function(){
+  console.log('Serial Port Opend');
+  serialport.on('data', function(data){
+      console.log(data);
   });
-
 });
-
-
-
 
 // handle web server
 function handler (req, res) {
@@ -82,97 +46,45 @@ function handler (req, res) {
 
 function calcSpeed(throtte, IsBrake, dist1, dist2){
 	//throtte = -50~50
-	
-	if(dist1 > safe_distance && dist2 > safe_distance){
-		// safe
-		speed_max = 100;
-		speed_min = 0;
-	}else{
-		if(dist1 < 10){
-			speed_max = 50+safe_speed;
-		}else if(dist1 < safe_distance){
-			speed_max = 50+safe_speed;
-			
-		}
-		
-		if((dist2 < 10){
-			speed_min = 50-safe_speed;
-		}else if(dist2 < safe_distance){
-			speed_min = 50-safe_speed;
-		}
-	}
-		
-	speed = (IsBrake-1)*throtte+50;
-	if( 50 > speed && speed < speed_min){
-		speed = speed_min;
-	}else if(speed > 50 && speed > speed_max){
-		speed = speed_max;
-	}
-	
+	speed = (IsBrake-1)*throtte*500+1500; //1000~2000, middle = 1500
 	return speed;
 }
 // on a socket connection
 io.sockets.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
  
-  // if board is ready
-  if(board.isReady){
-    // read in sensor data, pass to browser
-    sensor.on("data",function(){
-      socket.emit('sensor', { raw: this.raw });
-    });
-  }
 
-  // if servo message received
-  //socket.on('servo', function (data) {
-  //  console.log(data);
- //   if(board.isReady){ servo.to(data.pos);  }
- // });
   // if led message received
   socket.on('led', function (data) {
     console.log(data);
-     if(board.isReady){    led.strobe(data.delay); } 
   });
   
   // using gamepad data to control throtte and servo
 
   // throtte and steer
   socket.on('axes', function(data) {
-	console.log('xx:',data.xx,', xy:',data.xy,', yx:',data.yx,', yy:',data.yy);
-	throtte = -data.xy*50; // -50~50
+	//console.log('xx:',data.xx,', xy:',data.xy,', yx:',data.yx,', yy:',data.yy);
+	throtte = data.yy; // -50~50
 	speed = calcSpeed(throtte, IsBrake);
-	steer = (data.xx)*50+90; // 40~140
+	steer = (data.xx)*25+90; // 65~115
 
   });
   socket.on('A', function(data) {
 	console.log(data);
-	if( board.isReady){
-		//if no collision
-		//if()
-		// 根據蘑菇頭決定前進速度
-		esc.speed(speed)
 
-	}
   });
   // brake
   socket.on('X', function(data) {
   	IsBrake = 1;
-  	speed = 50;
-	esc.speed(speed);
-/*	console.log(data);
-	if( board.isReady){
-		speed = 50;
-		esc.speed(speed);
-	}*/
+
   });
 });
 
 // send command to arduino
 setInterval(function(){
-	if( board.isReady){
-		servo.to(steer);
-		esc.speed(speed);
-		IsBrake = 0; // reset to 0 every cycle
-		//console.log('speed:',speed,' steer:',steer);
-	}
+	string = 'S'+Math.round(steer)+'E'+Math.round(speed)+';';
+	serialport.write(string);
+	//console.log(string);
+	IsBrake = 0;
+	
 },50);
